f6617f5bcc3a2a429f91685df1a8ddd2a723a493
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-22 14:08:38 +0100

    insignificant

--- mongo_queries/adhoc/remove_hanging_requests.js
+++ mongo_queries/adhoc/remove_hanging_requests.js
@@ -1,10 +1,9 @@
 db.requests.update(
   {
-  answered:false,
   stopped:false,
   // ms * seconds * minutes *hour
-  // 3 hours ago
-  created_at:{$lt: new Date(ISODate().getTime() - 1000 * 60 * 60 * 3)}},
+  // 1 hour ago
+  created_at:{$lt: new Date(ISODate().getTime() - 1000 * 60 * 60 * 1)}},
   {
     $set: { stopped: true },
   },
