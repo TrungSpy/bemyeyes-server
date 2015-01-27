d19b0626a55583103464c631006711b95ac63154
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-27 19:55:58 +0100

    abort thread when requests stopped or answered"

--- helpers/i_find_helpers_for_request.rb
+++ helpers/i_find_helpers_for_request.rb
@@ -11,6 +11,10 @@ module BME
     def find_helpers_for_request request
       40.times do
         TheLogger.log.info "thread finding helpers for request #{request}"
+        request.reload
+       if request.stopped || request.answered
+         Thread.exit
+       end
         @requests_helper.check_request request, 1
         TheLogger.log.info "finished finding helpers"
         sleep 4 # seconds
