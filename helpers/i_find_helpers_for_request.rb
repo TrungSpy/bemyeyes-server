c72f1d5981925118e96d0d8c5799525230afafc3
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-27 18:45:08 +0100

    wow typo

--- helpers/i_find_helpers_for_request.rb
+++ helpers/i_find_helpers_for_request.rb
@@ -1,22 +1,7 @@
 module BME
-  class Timer
-    def self.time(&block)
-      start_time = Time.now
-      result = block.call
-      end_time = Time.now
-      @time_taken = end_time - start_time
-      result
-    end
-
-    def self.elapsedTime
-      return @time_taken
-    end
-
-  end
-
   class IFindHelpersForRequest
     def initialize requests_helper
-      @request_helper = requests_helper
+      @requests_helper = requests_helper
     end
 
     def start request
@@ -26,8 +11,8 @@ module BME
     def find_helpers_for_request request
       10.times do
         TheLogger.log.info "thread finding helpers for request #{request}"
-        Timer.time {@requests_helper.check_request request, 1}
-        TheLogger.log.info "finished finding helpers time elapsed: #{Timer.elapsedTime}"
+        @requests_helper.check_request request, 1
+        TheLogger.log.info "finished finding helpers"
         sleep 4 # seconds
       end
     end
