1ea2fa044991006087f865c4fe0e136121c1c047
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-01-27 17:46:06 +0100

    time how long it takes to find helper

--- helpers/i_find_helpers_for_request.rb
+++ helpers/i_find_helpers_for_request.rb
@@ -1,4 +1,19 @@
 module BME
+  class Timer
+    def self.time(&block)
+      start_time = Time.now
+      result = block.call
+      end_time = Time.now
+      @time_taken = end_time - start_time
+      result
+    end
+
+    def self.elapsedTime
+      return @time_taken
+    end
+
+  end
+
   class IFindHelpersForRequest
     def initialize requests_helper
       @request_helper = requests_helper
@@ -11,7 +26,8 @@ module BME
     def find_helpers_for_request request
       10.times do
         TheLogger.log.info "thread finding helpers for request #{request}"
-        @requests_helper.check_request request, 1
+        Timer.time {@requests_helper.check_request request, 1}
+        TheLogger.log.info "finished finding helpers time elapsed: #{Timer.elapsedTime}"
         sleep 4 # seconds
       end
     end
