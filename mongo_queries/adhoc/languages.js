aecf9f1b082785ab5266e5171a51065c25b240bc
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-02-23 11:37:20 +0100

    no print

--- mongo_queries/adhoc/languages.js
+++ mongo_queries/adhoc/languages.js
@@ -8,12 +8,10 @@ languages = db.users.distinct( 'languages' );
 _.forEach(languages, function(language) {
   var helpers_count = db.users.count( { languages: { $in: [ language ] }, role:"helper" } );
   var blind_count = db.users.count( { languages: { $in: [ language ] }, role:"blind" } );
-  print(language);
-  print(helpers_count);
+
   cnt = { helper_count:helpers_count, language:language, blind_count:blind_count};
   language_count.push(cnt);
 });
 
-language_count
 _.sortBy(language_count, function(count) { return count.helper_count });
 
