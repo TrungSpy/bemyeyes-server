0da51986e7cf55796201ba494205cf135291d8e9
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-03-18 14:48:34 +0100

    first attempt at finding duplicat helpers in a call

---
+++ mongo_queries/adhoc/find_duplicate_helper_in_call.js
@@ -0,0 +1,19 @@
+var dt = new Date()
+dt.setMonth(dt.getMonth()-1)
+
+db.event_logs.aggregate([
+      {$match:{name:"request_answered", created_at:{$gte:dt}}},
+      {$unwind:"$event_log_objects"},
+      {$match:{"event_log_objects.name":"request_id"}},
+      {$project:{ _id:"$event_log_objects", "day_of_year":{"$dayOfYear":"$created_at"},}},
+      { $group: {
+        _id: { request_id :"$_id.json_serialized", day_of_year:"$day_of_year"},
+        count: { $sum: 1 }
+      }},
+
+      { $match: {
+        count: { $gt: 1 }
+      }},
+    ])
+
+
