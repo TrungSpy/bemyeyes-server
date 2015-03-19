47906f940e23b64c7c4319d10a0a9ab18614918e
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-03-19 10:16:56 +0100

    getting ready to add more langauges

--- routes/languages.rb
+++ routes/languages.rb
@@ -1,11 +1,11 @@
 require 'language_list'
-class BMELanguage < Struct.new(:name, :iso_639_1)
+class BMELanguage < Struct.new(:name, :iso_639_1, :iso_639_3)
 end
 class App < Sinatra::Base
   register Sinatra::Namespace
   namespace '/languages' do
     get '/common' do
-      LanguageList::COMMON_LANGUAGES.reject{|language|!( language.living?)}.map{|lan| BMELanguage.new lan.name, lan.iso_639_1}.to_json
+      LanguageList::LIVING_LANGUAGES.map{|lan| BMELanguage.new lan.name.force_encoding("utf-8"),  lan.iso_639_1 ||  lan.iso_639_3, lan.iso_639_3}.to_json
     end
   end
 end
