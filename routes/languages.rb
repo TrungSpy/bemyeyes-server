94953309f41bd6830b68f7bf77ed68ebc1f1f56e
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-03-19 12:34:27 +0100

    add endpoint for living languages

--- routes/languages.rb
+++ routes/languages.rb
@@ -5,7 +5,12 @@ class App < Sinatra::Base
   register Sinatra::Namespace
   namespace '/languages' do
     get '/common' do
+      LanguageList::COMMON_LANGUAGES.reject{|language|!( language.living?)}.map{|lan| BMELanguage.new lan.name, lan.iso_639_1}.to_json
+    end
+    
+     get '/living' do
       LanguageList::LIVING_LANGUAGES.map{|lan| BMELanguage.new lan.name.force_encoding("utf-8"),  lan.iso_639_1 ||  lan.iso_639_3, lan.iso_639_3}.to_json
     end
+
   end
 end
