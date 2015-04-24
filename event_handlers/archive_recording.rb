5d23316266cd7568f42a0b8cebdde181428b3b68
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-04-24 10:31:13 +0200

    fixing up

--- event_handlers/archive_recording.rb
+++ event_handlers/archive_recording.rb
@@ -3,16 +3,25 @@ require_relative './event_handler_base'
 
 
 class ArchiveRecording < EventHandlerBase
+  def opentok
+    opentok_config = settings['opentok']
+    @opentok ||= OpenTok::OpenTok.new opentok_config['api_key'], opentok_config['api_secret']
+    @opentok
+  end
+
   def start payload
     begin
       @payload = payload
 
-      unless settings[:record_sessions]
-        TheLogger.log.info "Starting session recording #{request.session_id}"
+      unless settings["record_sessions"]
+       TheLogger.log.info "settings #{settings}"
         return
       end
+       TheLogger.log.info "Starting session recording #{request.session_id}"
 
       archive = opentok.archives.create(request.session_id, :name => "helper: " + helper.first_name)
+      TheLogger.log.info "Archive id: #{ archive.id}"
+
       request.additional_info[:archive_id] = archive.id
       request.save!
     rescue Exception => e
@@ -24,10 +33,10 @@ class ArchiveRecording < EventHandlerBase
     begin
       @payload = payload
 
-      unless settings[:record_sessions]
-        TheLogger.log.info "Stopping session recording #{request.session_id}"
+      unless settings["record_sessions"]
         return
       end
+      TheLogger.log.info "Stopping session recording #{request.session_id}"
 
       opentok.archives.stop_by_id request.additional_info[:archive_id]
     rescue Exception => e
