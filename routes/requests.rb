aacd1cce958ffef3afe69a8611668a90ffc23e3f
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-04-27 09:38:58 +0200

    make sure we return here - should not be ness. with the halt statement

--- routes/requests.rb
+++ routes/requests.rb
@@ -56,9 +56,11 @@ class App < Sinatra::Base
       if request.answered
         EventBus.announce(:try_answer_request_but_already_answered, request_id: request.id, helper:current_helper)
         give_error(400, ERROR_REQUEST_ALREADY_ANSWERED, "The request has already been answered.").to_json
+        return
       elsif request.stopped
         EventBus.announce(:try_answer_request_but_already_stopped, request_id: request.id, helper:current_helper)
         give_error(400, ERROR_REQUEST_STOPPED, "The request has been stopped.").to_json
+        return
       else
         request.answered = true
         request.helper = current_helper
