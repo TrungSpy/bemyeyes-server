6884434ac8a8e80d8e26e36e455b62cad57ee6a5
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-04-28 10:57:26 +0200

    record 2 seconds later

--- event_handlers/archive_recording.rb
+++ event_handlers/archive_recording.rb
@@ -1,6 +1,24 @@
 require 'opentok'
+require 'sucker_punch'
 require_relative './event_handler_base'
 
+class RecordingJob
+  include SuckerPunch::Job
+
+  def perform(opentok,request)
+    helper = request.helper
+
+    archive = opentok.archives.create(request.session_id, :name => "helper: " + helper.first_name)
+    TheLogger.log.info "Starting session recording #{request.session_id} Archive id: #{ archive.id}"
+    request.reload
+    request.additional_info[:archive_id] = archive.id
+    request.save!
+  end
+
+  def later(sec, opentok, request)
+    after(sec) { perform(opentok, request) }
+  end
+end
 
 class ArchiveRecording < EventHandlerBase
   def opentok
@@ -14,12 +32,8 @@ class ArchiveRecording < EventHandlerBase
       @payload = payload
 
       return unless settings["record_sessions"]
+      RecordingJob.new.async.later(2,opentok, request)
 
-      archive = opentok.archives.create(request.session_id, :name => "helper: " + helper.first_name)
-      TheLogger.log.info "Starting session recording #{request.session_id} Archive id: #{ archive.id}"
-      request.reload
-      request.additional_info[:archive_id] = archive.id
-      request.save!
     rescue Exception => e
       rescue_with_handler(e)
     end
