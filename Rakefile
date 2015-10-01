ab9a6bdee77db712859056934e24c90c5a6c7484
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-10-01 15:24:49 +0200

    reindent

--- Rakefile
+++ Rakefile
@@ -24,16 +24,16 @@ task :add_signup_points_to_all_helpers do
   Helper.find_each() do |helper|
     puts helper
     signup_found = false
-    helper.helper_points.each() do |hp| 
+    helper.helper_points.each() do |hp|
       if(hp.message == "signup")
         signup_found = true
       end
       puts hp.message
       puts signup_found
     end
-    
+
     if(!signup_found)
-      helper_point = HelperPoint. signup()  
+      helper_point = HelperPoint. signup()
       helper.helper_points.push helper_point
       helper.save
     end
--- models/helper.rb
+++ models/helper.rb
@@ -34,53 +34,53 @@ class Helper < User
 
   def waiting_requests
     request_ids = HelperRequest
-    .where(:helper_id => _id, :cancelled => false)
-    .fields(:request_id)
-    .all
-    .collect(&:request_id)
+      .where(:helper_id => _id, :cancelled => false)
+      .fields(:request_id)
+      .all
+      .collect(&:request_id)
     Request.all(:_id => {:$in =>request_ids}, :stopped => false, :answered  => false)
   end
 
   def available request=nil, limit=5
     begin
       raise 'no blind person in call' if request.blind.nil?
-       languages_of_blind = request.blind.languages ||["en"]
-       now = Time.now.utc
-       now_in_seconds_since_midnight = Helper.time_to_seconds_since_midnight now, 0
+      languages_of_blind = request.blind.languages ||["en"]
+      now = Time.now.utc
+      now_in_seconds_since_midnight = Helper.time_to_seconds_since_midnight now, 0
 
       request_id = request.present? ? request.id : nil
       contacted_helpers = HelperRequest
-      .where(:request_id => request_id)
-      .fields(:helper_id)
-      .all
-      .collect(&:helper_id) || []
+        .where(:request_id => request_id)
+        .fields(:helper_id)
+        .all
+        .collect(&:helper_id) || []
       contacted_helpers.compact!
       TheLogger.log.debug "contacted_helpers #{contacted_helpers}"
 
-       helpers_in_a_call = Request.running_requests
-      .fields(:helper_id)
-      .all
-      .collect(&:helper_id) || []
+      helpers_in_a_call = Request.running_requests
+        .fields(:helper_id)
+        .all
+        .collect(&:helper_id) || []
       helpers_in_a_call.compact!
       TheLogger.log.debug "helpers_in_a_call #{helpers_in_a_call}"
 
-        Helper.where(
-      :id.nin => contacted_helpers,
-      "$or" => [
-        {:available_from => nil},
-        {:available_from.lt => Time.now.utc}
-    ])
-    .where(
-      :go_to_sleep_in_seconds_since_midnight.gte => now_in_seconds_since_midnight,
-      :wake_up_in_seconds_since_midnight.lte => now_in_seconds_since_midnight)
-    .where('abuse_reports.blind_id' => {"$ne" =>  request.blind_id})
-    .where(:expiry_time.gt => Time.now)
-    .where(:blocked => false)
-    .where(:languages => {:$in => languages_of_blind})
-    .where(:user_id.nin => helpers_in_a_call)
-    .where(:inactive => false)
-    .sort(:last_help_request.asc)
-    .all.sample(limit)
+      Helper.where(
+        :id.nin => contacted_helpers,
+        "$or" => [
+          {:available_from => nil},
+          {:available_from.lt => Time.now.utc}
+        ])
+        .where(
+        :go_to_sleep_in_seconds_since_midnight.gte => now_in_seconds_since_midnight,
+        :wake_up_in_seconds_since_midnight.lte => now_in_seconds_since_midnight)
+        .where('abuse_reports.blind_id' => {"$ne" =>  request.blind_id})
+        .where(:expiry_time.gt => Time.now)
+        .where(:blocked => false)
+        .where(:languages => {:$in => languages_of_blind})
+        .where(:user_id.nin => helpers_in_a_call)
+        .where(:inactive => false)
+        .sort(:last_help_request.asc)
+        .all.sample(limit)
     rescue Exception => e
       TheLogger.log.fatal "Fatal trying to find available helpers #{e.message}"
       []
